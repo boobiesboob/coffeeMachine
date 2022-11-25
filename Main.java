a99658b5db6376299f05d2c8c3a0ac412e4f0259
Mo$he <boobiesboob@gmx.net>
2022-11-25 06:36:24 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     /**
      * would do this project as next ASAP after another stuff.
      * now just started it to not forget Git.
-     * if you see read this, I wish to a good day!
+     * if you read this, I wish to a good day!
      * @param args
      */
     public static void main(String[] args) {
