c0253053b36372fda630dbd36c99108da76caddb
Mo$he <e.bubnov@gmx.de>
2022-11-25 06:20:57 +0100

    hodl the space for a new coffee machine project

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /**
+     * would do this project as next ASAP after another stuff.
+     * now just started it to not forget Git.
+     * if you see read this, I wish to a good day!
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Coffee!");
+    }
+}
