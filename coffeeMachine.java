02bd022888aff0409cdd0d4bf7596b9921f2fd9d
Mo$he <boobiesboob@gmx.net>
2023-03-26 16:12:10 +0200

    Update and rename CoffeeMachinePart1.java to coffeeMachine.java
    renamed class and project name

--- coffeeMachine.java
+++ coffeeMachine.java
@@ -1,6 +1,6 @@
 package machine;
 
-public class CoffeeMachine {
+public class CoffeeMachinePart1 {
     public static void main(String[] args) {
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
